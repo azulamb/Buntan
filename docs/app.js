dc10bd93a3670330d81e67595fc86a7f2140c8cf
Hiroki <1127785+azulamb@users.noreply.github.com>
2018-04-03 17:43:01 +0900

    Add first version.

---
+++ docs/app.js
@@ -0,0 +1,183 @@
+var Buntan = (function () {
+    function Buntan(target, config) {
+        this.logs = [];
+        this.anime = false;
+        this.effect = false;
+        this.effects = {};
+        this.count = -1;
+        this.charInterval = 0;
+        this.autoInterval = 0;
+        this.events = {};
+        this.config = config || {};
+        if (this.config.escapement && this.config.escapement < 0) {
+            this.config.escapement = 0;
+        }
+        this.chara = target.getElementsByClassName('chara')[0];
+        var win = target.getElementsByClassName('msgwin')[0];
+        this.name = win.getElementsByClassName('name')[0];
+        this.text = win.getElementsByClassName('text')[0];
+    }
+    Buntan.prototype.addEffect = function (name, effect) {
+        this.effects[name] = effect;
+    };
+    Buntan.prototype.addEventListener = function (event, cb) {
+        if (!cb) {
+            return;
+        }
+        switch (event) {
+            case 'choices':
+                this.events.choices = cb;
+                break;
+            case 'next':
+                this.events.next = cb;
+                break;
+            case 'end':
+                this.events.end = cb;
+                break;
+        }
+    };
+    Buntan.prototype.onChoices = function () {
+        if (!this.events.choices) {
+            return;
+        }
+        this.events.choices(this.logs[this.count].choices || []);
+    };
+    Buntan.prototype.onNext = function (prev, next) {
+        if (!this.events.next) {
+            return;
+        }
+        this.events.next(this.logs[prev], this.logs[next]);
+    };
+    Buntan.prototype.onEnd = function (end) {
+        if (!this.events.end) {
+            return;
+        }
+        this.events.end(this.logs[end]);
+    };
+    Buntan.prototype.add = function (logs) {
+        var _this = this;
+        logs.forEach(function (log) { _this.logs.push(log); });
+    };
+    Buntan.prototype.nextLog = function (label) {
+        var count = this.count;
+        if (!label) {
+            this.onNext(count, count + 1);
+            return this.logs[++this.count];
+        }
+        this.count = -1;
+        while (++this.count < this.logs.length) {
+            if (this.logs[this.count].label === label) {
+                this.onNext(count, this.count);
+                return this.logs[this.count];
+            }
+        }
+        this.onEnd(count);
+        return null;
+    };
+    Buntan.prototype.clearCharAnime = function () {
+        var _this = this;
+        if (this.charInterval) {
+            clearInterval(this.charInterval);
+        }
+        this.charInterval = 0;
+        while (0 < this.chars.length) {
+            this.text.textContent += this.chars.shift() || '';
+        }
+        if (this.effect) {
+            return;
+        }
+        var key = this.logs[this.count].effect;
+        if (key && this.effects[key]) {
+            this.effect = true;
+            this.effects[key](function () {
+                _this.effect = false;
+                _this.anime = false;
+            });
+            return;
+        }
+        this.anime = false;
+    };
+    Buntan.prototype.nextChar = function () {
+        var _this = this;
+        if (!this.config.escapement || this.config.escapement <= 0) {
+            this.clearCharAnime();
+            return;
+        }
+        this.charInterval = setInterval(function () {
+            if (_this.chars.length <= 0) {
+                return _this.clearCharAnime();
+            }
+            _this.text.textContent += _this.chars.shift() || '';
+        }, this.config.escapement);
+    };
+    Buntan.prototype._next = function (log) {
+        if (log.name !== undefined) {
+            this.name.textContent = log.name;
+        }
+        this.chars = log.text.split(/(?![\uDC00-\uDFFF])/);
+        this.text.textContent = '';
+        this.anime = true;
+    };
+    Buntan.prototype.next = function (label) {
+        if (this.anime) {
+            if (0 < this.chars.length) {
+                this.clearCharAnime();
+            }
+            return null;
+        }
+        if (this.logs.length <= this.count) {
+            return null;
+        }
+        if (0 <= this.count && this.logs[this.count].choices && !label) {
+            if (this.events.choices && this.logs[this.count].choices) {
+                this.onChoices();
+            }
+            return this.logs[this.count].choices;
+        }
+        if (0 <= this.count && this.logs[this.count].jump) {
+            label = this.logs[this.count].jump;
+        }
+        var log = this.nextLog(label);
+        if (!log) {
+            return null;
+        }
+        this._next(log);
+        this.nextChar();
+        return null;
+    };
+    Buntan.prototype.choice = function (select) {
+        if (this.count < 0 || !this.logs[this.count] || !this.logs[this.count].choices) {
+            return this.next();
+        }
+        var choices = this.logs[this.count].choices;
+        if (choices.length <= 1) {
+            return this.next(choices[0].label);
+        }
+        return this.next(choices[select % choices.length].label);
+    };
+    Buntan.prototype.auto = function () {
+        var _this = this;
+        if (this.autoInterval) {
+            return;
+        }
+        var count = 10;
+        this.autoInterval = setInterval(function () {
+            if (_this.anime) {
+                return;
+            }
+            if (0 < --count) {
+                return;
+            }
+            count = 10;
+            _this.next();
+        }, this.config.escapement || 100);
+    };
+    Buntan.prototype.cancelAuto = function () {
+        if (this.autoInterval) {
+            clearInterval(this.autoInterval);
+        }
+        this.autoInterval = 0;
+    };
+    Buntan.prototype.getLogs = function () { };
+    return Buntan;
+}());
---
+++ src/index.ts
@@ -0,0 +1,374 @@
+interface CHOICE_DATA
+{
+	name: string,
+	label: string ,
+}
+
+interface BUNTAN_DATA
+{
+	text: string,
+	name?: string,
+	img?: string,
+	back?: string,
+	label?: string,
+	jump?: string,
+	effect?: string,
+	choices?: CHOICE_DATA[],
+}
+
+interface BUNTAN_CONFIG
+{
+	escapement?: number,
+}
+
+interface BUNTAN_EVENT
+{ 
+	choices?: ( data: CHOICE_DATA[] ) => void,
+	next?: ( prev: BUNTAN_DATA, next: BUNTAN_DATA ) => void,
+	end?: ( last: BUNTAN_DATA ) => void,
+}
+
+class Buntan
+{
+	private logs: BUNTAN_DATA[];
+	private config: BUNTAN_CONFIG;
+
+	private chara: HTMLElement;
+	private name: HTMLElement;
+	private text: HTMLElement;
+
+	private anime: boolean;
+	private effect: boolean;
+	private count: number;
+	private chars: string[];
+	private charInterval: number;
+	private autoInterval: number;
+
+	private effects: { [ key: string ]: ( cb: () => void ) => void }
+	private events: BUNTAN_EVENT;
+
+	/** コンストラクタ
+	target: 必要なUIを含んだHTMLElement
+	config: 制御設定[任意]
+	        escapement: 文字送りの速度(ミリ秒)
+	*/
+	constructor( target: HTMLElement, config?: BUNTAN_CONFIG )
+	{
+		// もろもろの初期化
+		this.logs = [];
+		this.anime = false;
+		this.effect = false;
+		this.effects = {};
+		this.count = -1;
+		this.charInterval = 0;
+		this.autoInterval = 0;
+		this.events = {};
+		this.config = config || {};
+
+		// 制御設定の初期化
+		if ( this.config.escapement && this.config.escapement < 0 ) { this.config.escapement = 0; }
+
+		// 必要な要素の取得
+		this.chara = <HTMLElement>target.getElementsByClassName( 'chara' )[ 0 ];
+		const win = target.getElementsByClassName( 'msgwin' )[ 0 ];
+		this.name = <HTMLElement>win.getElementsByClassName( 'name' )[ 0 ];
+		this.text = <HTMLElement>win.getElementsByClassName( 'text' )[ 0 ];
+	}
+
+	/** エフェクト追加
+	name:   エフェクト名
+	effect: エフェクト関数
+	        必ずエフェクトの後に第一引数のcbを呼ぶようにすること！
+	*/
+	public addEffect( name: string, effect: ( cb: () => void ) => void )
+	{
+		this.effects[ name ] = effect;
+	}
+
+	/** イベント追加
+	event:  対応したイベントを追加できる
+	        choices ... 選択肢が現れた時に呼ばれるイベント
+	        next    ... 次の文章に進むときに呼ばれるイベント
+	        end     ... 文章の最後を再生し終わった時に呼ばれるイベント
+	cb:     イベントが発生したときに呼ばれるコールバック関数
+	*/
+	public addEventListener( event: 'choices', cb: BUNTAN_EVENT[ 'choices' ] ): void;
+	public addEventListener<K extends keyof BUNTAN_EVENT>( event: K, cb: BUNTAN_EVENT[ K ] )
+	{
+		if ( !cb ) { return; }
+		switch ( event )
+		{
+			case 'choices': this.events.choices = <BUNTAN_EVENT['choices']>cb; break;
+			case 'next': this.events.next = <BUNTAN_EVENT['next']>cb; break;
+			case 'end': this.events.end = <BUNTAN_EVENT['end']>cb; break;
+		}
+	}
+
+	private onChoices()
+	{
+		if ( !this.events.choices ) { return; }
+		this.events.choices( this.logs[ this.count ].choices || [] );
+	}
+
+	private onNext( prev: number, next: number )
+	{
+		if ( !this.events.next ) { return; }
+		this.events.next( this.logs[ prev ], this.logs[ next ] );
+	}
+
+	private onEnd( end: number )
+	{
+		if ( !this.events.end ) { return; }
+		this.events.end( this.logs[ end ] );
+	}
+
+	/**
+	logs:   会話ログを追加する
+	        以下構造の配列を与える
+	        {
+	            text:   会話文[必須]
+	            name:   表示する名前[任意,省略時には過去のを引き継ぐ]
+	            img:    表示するキャラの画像パス[任意]
+	            back:   表示する背景の画像パス[任意]
+	            label:  設定すると選択肢で飛ぶことができるラベル[任意]
+	            jump:   設定するとテキスト再生後に指定したラベルに飛ぶ[任意]
+	            effect: 設定するとテキスト再生後に指定したエフェクトを再生する[任意]
+	                    エフェクトは事前にaddEffectで登録する必要あり
+	            choices:選択肢の配列[任意]
+	                    { name: 表示選択肢, label: 遷移先ラベル }
+	        }
+	*/
+	public add( logs: BUNTAN_DATA[] )
+	{
+		logs.forEach( ( log ) => { this.logs.push( log ); } );
+	}
+
+	/** 次に進む
+	label:  飛ぶ先のラベル[任意]
+	*/
+	private nextLog( label?: string )
+	{
+		const count = this.count;
+		if ( !label )
+		{
+			this.onNext( count, count + 1 );
+			return this.logs[ ++this.count ];
+		}
+
+		// labelが設定してある場合、最初から検索する
+		this.count = -1;
+		while ( ++this.count < this.logs.length )
+		{
+			if ( this.logs[ this.count ].label === label )
+			{
+				this.onNext( count, this.count );
+				return this.logs[ this.count ];
+			}
+		}
+		this.onEnd( count );
+
+		return null;
+	}
+
+	/** 文字送りを止めてすべて表示する*/
+	private clearCharAnime()
+	{
+		if ( this.charInterval ) { clearInterval( this.charInterval ); }
+		this.charInterval = 0;
+		while ( 0 < this.chars.length ) { this.text.textContent += this.chars.shift() || ''; }
+		if ( this.effect ) { return; }
+		const key = this.logs[ this.count ].effect;
+		if ( key && this.effects[ key ] )
+		{
+			this.effect = true;
+			this.effects[ key ]( () =>
+			{
+				this.effect = false;
+				this.anime = false;
+			} );
+			return;
+		}
+		this.anime = false;
+	}
+
+	/** 文字送りを開始する */
+	private nextChar()
+	{
+		if ( !this.config.escapement || this.config.escapement <= 0 )
+		{
+			this.clearCharAnime();
+			return;
+		}
+
+		this.charInterval = setInterval( () =>
+		{
+			if ( this.chars.length <= 0 ) { return this.clearCharAnime(); }
+			this.text.textContent += this.chars.shift() || '';
+		}, this.config.escapement );
+	}
+
+	/** 次のログを表示する */
+	private _next( log: BUNTAN_DATA )
+	{
+		if ( log.name !== undefined ) { this.name.textContent = log.name; }
+
+		this.chars = log.text.split( /(?![\uDC00-\uDFFF])/ );
+		this.text.textContent = '';
+
+		this.anime = true;
+	}
+
+	/** 次に進む
+	label:  次に進むラベル[任意]
+	*/
+	public next( label?: string )
+	{
+		if ( this.anime )
+		{
+			// 文字送り中の場合は、文字送りをやめて全文表示
+			if ( 0 < this.chars.length ) { this.clearCharAnime(); }
+			return null;
+		}
+
+		// もうログがない場合は何もしない
+		if ( this.logs.length <= this.count ) { return null; }
+
+		// 今選択肢にいる場合、label無しで次に進ませない。
+		if ( 0 <= this.count && this.logs[ this.count ].choices && !label )
+		{
+			// イベントを登録している場合は実行
+			if ( this.events.choices && this.logs[ this.count ].choices ) { this.onChoices(); }
+			return this.logs[ this.count ].choices;
+		}
+
+		// 今飛び先にいる場合、強制的にラベルを書き換える。
+		if ( 0 <= this.count && this.logs[ this.count ].jump )
+		{
+			label = this.logs[ this.count ].jump;
+		}
+
+		// 次に進む
+		const log = this.nextLog( label );
+
+		if ( !log ) { return null; }
+
+		// 次に進めるので、表示する
+		this._next( log );
+
+		this.nextChar();
+
+		return null;
+	}
+
+	/** 選択肢を選ぶ
+	select: 選択肢に来ている場合、選択肢を番号で選べる
+	*/
+	public choice( select: number )
+	{
+		if ( this.count < 0 || !this.logs[ this.count ] || !this.logs[ this.count ].choices ) { return this.next(); }
+		const choices = <CHOICE_DATA[]>this.logs[ this.count ].choices;
+		if ( choices.length <= 1 )
+		{
+			return this.next( choices[ 0 ].label );
+		}
+		return this.next( choices[ select % choices.length ].label );
+	}
+
+	/** オートモード開始 */
+	public auto()
+	{
+		if ( this.autoInterval ) { return; }
+
+		let count = 10;
+		this.autoInterval = setInterval( () =>
+		{
+			if ( this.anime ) { return; }
+			if ( 0 < --count ) { return; }
+			count = 10;
+			this.next();
+		}, this.config.escapement || 100 );
+	}
+
+	/** オートモードをやめる */
+	public cancelAuto()
+	{
+		if ( this.autoInterval ) { clearInterval( this.autoInterval ); }
+		this.autoInterval = 0;
+	}
+
+	/** 次の選択肢までスキップする */
+	/*public skip()
+	{
+		TODO: onNext,onEnd
+		while ( this.count < this.logs.length )
+		{
+			if ( this.logs[ this.count ].choices ) { return this.onChoices(); }
+			++this.count;
+		}
+	}*/
+
+	public getLogs(){}
+
+}
+
+/*document.addEventListener( 'DOMContentLoaded', () =>
+{
+	const adv = new Buntan( <HTMLElement>document.getElementById( 'content' ),
+	{
+		escapement: 100,
+	} );
+	adv.addEffect( 'finish', ( end ) => {
+		const content = <HTMLElement>document.getElementById( 'content' );
+		const color = content.style.backgroundColor;
+		content.style.backgroundColor = 'pink';
+		setTimeout( () =>
+		{
+			content.style.backgroundColor = color;
+			end();
+		}, 2000 );
+	} );
+	adv.add(
+	[
+		{ name: 'aaa', text: 'もしかして？' },
+		{ name: 'bbb', text: '私たち？' },
+		{ name: '二人', text: '入れ替わってるー！？', choices: [ { name: 'はい', label: 'next' }, { name: 'いいえ', label: 'change' } ], label: 'change' },
+		{ name: 'ナレーション', text: '説明しよう！', label: 'next' },
+		{ text: '二人の体は一時的に入れ替わってしまったのだ！' },
+		{ text: '今後の二人の運命やいかに！', choices: [ { name: 'はい', label: 'next2' }, { name: 'いいえ', label: 'next3' } ] },
+		{ text: 'はいを選びました。', label: 'next2', jump: 'endif' },
+		{ text: 'いいえを選びました', label: 'next3', jump: 'endif' },
+		{ name: 'システム', text: '自爆システムを稼働します', label: 'endif', effect: 'finish' },
+		{ text: '自爆しました' },
+	] );
+
+	adv.addEventListener( 'choices', ( choices ) =>
+	{
+		const area = <HTMLElement>document.getElementById( 'choices' );
+		area.classList.add( 'open' );
+		const children = area.childNodes;
+		for ( let i = children.length - 1 ; 0 <= i ; --i ) { area.removeChild( children[ i ] ); }
+		choices.forEach( ( choice, index ) =>
+		{
+			const button = document.createElement( 'button' );
+			button.textContent = choice.name;
+			button.addEventListener( 'click', ( event ) =>
+			{
+				event.preventDefault();
+				adv.choice( index );
+				area.classList.remove( 'open' );
+			}, false );
+			area.appendChild( button );
+		} );
+	} );
+
+	(<HTMLElement>document.getElementById( 'next' )).addEventListener( 'click', ( event ) =>
+	{
+		event.preventDefault();
+		adv.cancelAuto();
+		const choices = adv.next();
+	}, false );
+	(<HTMLElement>document.getElementById( 'auto' )).addEventListener( 'click', ( event ) =>
+	{
+		adv.auto();
+	}, false );
+} );*/
